4083b596ef3c32612e161735b37795dd05dbfd0c
juan-miller <149274972+juan-miller@users.noreply.github.com>
2023-11-02 16:15:33 -0300

    Add files via upload
    Projeto de PI

---
+++ ProjetoPi.java
@@ -0,0 +1,35 @@
+package Projeto;
+import java.util.Scanner;
+
+public class ProjetoPi {
+    public static int menu (){
+        Scanner input = new Scanner(System.in);
+        int menu;
+
+        do {
+        System.out.println("*-* MENU *-* ");
+        System.out.println("\n1 - Novo Jogo");
+        System.out.println("2 - créditos dos criadores");
+        System.out.println("Digite o número equivalente a opção que desejar =D");
+        menu = input.nextInt();
+        if (menu < 1 || menu > 2){
+            System.out.println("Operação Inválida");
+            System.out.println("Escolha uma Operação");
+
+            input.close();
+        }
+        }while(menu < 1 || menu > 4);
+        return menu;
+    }
+    
+
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(menu());
+    }
+    
+}
