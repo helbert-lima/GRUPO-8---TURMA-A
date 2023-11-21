dbe009182064d90559366ef66fb6146ffa7d7d24
helbert-lima <149211706+helbert-lima@users.noreply.github.com>
2023-11-21 15:07:38 -0300

    testeFunçao.java

--- Ajust_calculadora.java
+++ Ajust_calculadora.java
@@ -2,6 +2,6 @@ package teste;
 
 public class Ajust_calculadora {
   public static void main(String[] args) {
-    System.out..println("só testando... tururu...");
+    System.out..println("só testando... tururu..."); //Usar este código para testar Função do Jogo!
   }
 }
